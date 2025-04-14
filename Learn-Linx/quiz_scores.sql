0aed35195778b12ada025ed98a58b848e424ad48
Marlene Habib <82187174+marleneh456@users.noreply.github.com>
2025-04-13 22:35:43 -0500

    Delete Learn-Linx directory

--- Learn-Linx/quiz_scores.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE quiz_scores (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(100),
-    course_name VARCHAR(100),
-    score INT,
-    total INT,
-    taken_on DATETIME DEFAULT CURRENT_TIMESTAMP
-);
